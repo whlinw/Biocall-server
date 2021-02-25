607a4eb537c88a7c966fe6360c5b07e96508fa51
WH Lin <whlin.w@gmail.com>
2021-02-25 01:21:17 +0100

    minor updates: add server function initRoom; support cmd args; improved loggin; function documentation; fix inconsistent syntax

--- Room.js
+++ Room.js
@@ -1,38 +1,66 @@
 class Room {
 	constructor(room, bioData) {
 		this.name = room;
-		this.bioData = {'gsr': 0.0}
+		this.bioData = {'gsr': 0.0};
 		this.showClient = {'Border': false, 'Stress': false};
 		this.spoof = {'gsr': {on: false, value:1}}
 		this.numConnections = 0;
 		this.connections = [];
 	}
+
+	/**
+	 * Get the room data for initializing room settings.
+	 * @return {Object}			Room data including spoof & feedback settings.
+	 */
+	getRoomData() {
+		let data = {spoof: this.spoof, showClient: this.showClient};
+		return data;
+	}
 	
 	/**
-	 * Set BioData of name with a single value
-	 * @param {string} name - Name of the data
-	 * @param {TYPE} value - Data value
+	 * Set the latest biodata of a given type.
+	 * @param {string} type		Type of the data. e.g., gsr
+	 * @param {string} value	Data value.
 	 */
-	setBioData(name, value) {
+	setBioData(type, value) {
 		let val = parseFloat(value);
-		this.bioData[name] = val;
+		this.bioData[type] = val;
 	}
 
-	getBioData(name) {
-		if(typeof name==='undefined') {
+	/**
+	 * Get the latest value of a given biodata.
+	 * @param {string} type		Type of the data.
+	 * @return {string} 		The latest value of biodata of the given type.
+	 */
+	getBioData(type) {
+		if(typeof type==='undefined') {
 			return this.bioData;
 		}
-		return this.bioData[name];
+		return this.bioData[type];
 	}
 
-	setSpoof(name, bool) {
-		this.spoof[name].on=bool;
+	/**
+	 * Set the current status of the spoof function for the biodata of a given type.
+	 * @param {string} type		Type of the data.
+	 * @param {bool} bool		True/false if the spoof function should be on/off.
+	 */
+	setSpoof(type, bool) {
+		this.spoof[type].on=bool;
 	}
 
-	setSpoofValue(name, val) {
-		this.spoof[name].value=val;
+	/**
+	 * Set the current spoof value for the biodata of a given type.
+	 * @param {string} type		Type of the data.
+	 * @param {float} val		Spoof value.
+	 */
+	setSpoofValue(type, val) {
+		this.spoof[type].value=val;
 	}
 
+	/**
+	 * Add an ID and increase the number of connection in the room.
+	 * @param {string} id		Connection ID.
+	 */
 	addConnection(id) {
 		if (!this.connections.includes(id)) {
 			this.connections.push(id);
@@ -40,6 +68,10 @@ class Room {
 		}
 	}
 
+	/**
+	 * Remove an ID and decrease the number of connection in the room.
+	 * @param {string} id		Connection ID.
+	 */
 	removeConnection(id) {
 		this.connections.splice(this.connections.indexOf(id), 1);
 		this.numConnections -= 1;
--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 const Room = require('./Room');
 
-const http = require('http').createServer();
-const io = require('socket.io')(http);
+var argv = require('minimist')(process.argv.slice(2));
 
-const PORT = 4001
+var app = require('http').createServer();
+if (argv.https){
+	var fs = require('fs');
+	var key = './cert/pvkey.pem';
+	var cert = './cert/cert.crt';
+	if (argv.key) { key = argv.key; }
+	if (argv.cert) { cert = argv.cert; }
+	var options = {
+		key: fs.readFileSync(key),
+		cert: fs.readFileSync(cert)
+	}
+	app = require('https').createServer(options);
+}
+const io = require('socket.io')(app);
 
-var rooms = {}
-var guestList = {}
+var PORT = 4001;
+if (argv.port){
+	PORT = argv.port;
+}
 
+var rooms = {};
+var guestList = {};
+
+/**
+ * Add a connection to a room. A new room is created if the room is not existed.
+ * @param {string} id		Connection ID.
+ * @param {string} room		Name of the room.
+ * @param {Object} socket	Socket object of the connection.
+ * @return {int}			Number of connection in the room.
+ */
 function joinRoom(id, room, socket) {
 	if (!(room in rooms)) {
 		rooms[room] = new Room(room);
@@ -15,16 +39,32 @@ function joinRoom(id, room, socket) {
 		sendData(room, socket);
 	} else {
 		rooms[room].addConnection(id);
+		let roomData = rooms[room].getRoomData();
+		socket.emit('initRoom', roomData);
 	}
 	guestList[id] = room;
+	return rooms[room].numConnections;
 }
 
+/**
+ * Set the biodata of a given type of a room.
+ * @param {string} type		Type of the data.
+ * @param {string} room		Name of the room.
+ * @param {string} data		Data value.
+ */
 function addData(type, room, data) {
-	// console.log('New data:', room, data);
 	rooms[room].setBioData(type, data);
 }
 
+/**
+ * Send all the latest biodata to all users in a room.
+ * @param {string} room		Name of the room.
+ * @param {Object} socket	Socket object of the user that provides biodata.
+ */
 function sendData(room, socket) {
+	if (typeof(rooms[room])==='undefined') {
+		return;
+	}
 	let bioData = rooms[room].getBioData();
 	io.to(room).emit('bioData', bioData);
 
@@ -34,69 +74,79 @@ function sendData(room, socket) {
 		let minTop = spoofedInput-0.1;
 		let randVal = Math.random() * (maxTop - minTop) + minTop;
 		let spoofDataGSR = randVal.toFixed(2);
-		io.to(room).emit('spoofDataGSR', spoofDataGSR)
+		io.to(room).emit('spoofDataGSR', spoofDataGSR);
 	}
 
 	if (socket.connected) {
 		if (room in rooms && rooms[room].numConnections > 0) {
-			setTimeout(sendData, 1000, room, socket)
+			setTimeout(sendData, 1000, room, socket);
 		}
 	}
 }
 
-function setSpoofBorder(room, bool) {
-	console.log('Started spoofing: ' + bool);
-	rooms[room].setSpoof('border', bool);
-	io.to(room).emit('spoofBorder', bool);
-}
-
-function setSpoofValue(room, data) {
-	rooms[room].setSpoofValue('border', parseFloat(data));
-}
-
+/**
+ * Start/Stop the GSR spoof function of a room.
+ * @param {string} room		Name of the room.
+ * @param {bool} bool		True/false for start/stop.
+ */
 function setSpoofGSR(room, bool) {
   rooms[room].setSpoof('gsr', bool);
-  io.to(room).emit('setSpoofGSR', bool)
+  io.to(room).emit('setSpoofGSR', bool);
 }
 
+/**
+ * Set the GSR spoof value of a room.
+ * @param {string} room		Name of the room.
+ * @param {string} data		Spoof value.
+ */
 function setSpoofValueGSR(room, data) {
 	rooms[room].setSpoofValue('gsr', parseFloat(data));
 }
 
-function setClientShowing(room, element, value) {
-	// console.log('setClientShowing:', element, value);
-	rooms[room].showClient[element] = value;
-	io.to(room).emit('showToClient' + element, value);
+/**
+ * Set biofeedback to be visible/invisible to the client of a room. 
+ * @param {string} room		Name of the room.
+ * @param {string} type		Type of feedback. 
+ * @param {bool} bool		Data value.
+ */
+function setClientShowing(room, type, bool) {
+	rooms[room].showClient[type] = bool;
+	io.to(room).emit('showToClient' + type, bool);
 }
 
 io.on('connection', (socket) => {
-	console.log('New connection:', socket.id);
+	console.log('[Log] New socket connection:', socket.id);
 
 	socket.on('joinRoom', room_name => {
-		joinRoom(socket.id, room_name, socket);
+		let numConn = joinRoom(socket.id, room_name, socket);
 		socket.join(room_name);
-		console.log('User', socket.id, 'joined Room', room_name);
-		console.log('Current rooms:', rooms);
+		console.log('[Log] Client "' + socket.id + '" joined room "' + room_name + '". Connections in room "' + room_name + '": ' + numConn);
+		if (numConn == 1) {
+			console.log('[Log] New room "' + room_name + '" created. Current rooms:', Object.keys(rooms));
+		}
 	});
 
 	socket.on('disconnect', () => {
-		let room = guestList[socket.id]
+		let room = guestList[socket.id];
 		try {
 			rooms[room].removeConnection(socket.id);
 			delete guestList[socket.id];
 		}
 		catch(err) {
-			console.log('Error:', err);
+			console.log('[Err] Error:', err);
 		}
-		checkRoom(room);
-		console.log('Socket.io: Client disconnected on port ' + PORT);
+		console.log('[Log] Client "' + socket.id + '" from room "' + room + '" disconnected. Connections in room "' + room + '": ' + rooms[room].numConnections);
+
+		// Check and delete the room if there is no connection in a room.
+		if (rooms[room].numConnections==0) {
+			delete rooms[room];
+			console.log('[Log] Room "' + room + '" removed. Current rooms:', Object.keys(rooms));
+		}
 	});
 
 	socket.on('gsrData', data => {addData('gsr', Object.keys(socket.rooms)[1], data); });
 	socket.on('hrData', data => {addData('hr', Object.keys(socket.rooms)[1], data); });
 
-	socket.on('spoofBorder', bool => { setSpoofBorder(Object.keys(socket.rooms)[1], bool) });
-	socket.on('spoofValue', value => { setSpoofValue(Object.keys(socket.rooms)[1], value) });
 	socket.on('spoofGSR', bool => { setSpoofGSR(Object.keys(socket.rooms)[1], bool) });
 	socket.on('spoofValueGSR', value => { setSpoofValueGSR(Object.keys(socket.rooms)[1], value) });
 
@@ -104,16 +154,6 @@ io.on('connection', (socket) => {
 	socket.on('showToClientStress', data => {setClientShowing(Object.keys(socket.rooms)[1], 'Stress', data)});
 });
 
-function checkRoom(room) {
-	if (rooms[room].numConnections==0) {
-		delete rooms[room];
-	}
-}
-
-http.listen(PORT, () => {
-	console.log('Listening on port', PORT);
-});
-
-process.on('uncaughtException', function (err) {
-	console.error(err);
+app.listen(PORT, () => {
+	console.log('[Log] Server started. Listening on port', PORT);
 });
